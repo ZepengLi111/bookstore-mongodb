dd8f702a9e998119cbbdb14e9de5be7844b5afb7
lzp <1224060318@qq.com>
2023-11-10 21:58:46 +0800

    Fix: delete useless code in seller.py

--- be/model/seller.py
+++ be/model/seller.py
@@ -106,8 +106,6 @@ class Seller(db_conn.DBConn):
                 return error.error_order_state(result_order['state'])
             else:
                 result = self.order.update_one({"order_id": order_id, "seller_store_id": store_id}, {"$set": {"state": 2}})
-                return 200, "ok"
-            
 
         except errors.PyMongoError as e:
             return 528, "{}".format(str(e))
